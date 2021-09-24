ab0cbce19ddc1b5681779231c8521eddd3dbff95
AR_Sunny <dev.arsunnybd@gmail.com>
2021-09-24 23:18:14 +0600

    Print an integer number

---
+++ test.c
@@ -0,0 +1,16 @@
+/* An integer vabiable num,that we contain 10;
+write a program which prints an integer nunmber.
+*/
+
+#include <stdio.h>
+
+int main()
+
+{
+    int num = 10;
+
+    printf("The number is %d", num);
+    
+    return 0;
+
+}
