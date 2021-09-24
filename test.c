a35e4722c40096a0720aa026742ea1e7ce3336bc
AR_Sunny <dev.arsunnybd@gmail.com>
2021-09-24 23:29:12 +0600

    Write two numbers in same time

--- test.c
+++ test.c
@@ -7,10 +7,15 @@ write a program which prints an integer nunmber.
 int main()
 
 {
-    int num = 10;
+   /* int num = 10;
 
     printf("The number is %d", num);
-    
+    */
+
+   int num1 = 10, num2 = 20;
+
+    printf("Numbers are %d, %d\n",num1, num2);
+   
     return 0;
 
 }
