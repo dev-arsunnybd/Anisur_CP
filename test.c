5a8dd422c0ffd47190a743116b67be43829fdcaa
AR_Sunny <dev.arsunnybd@gmail.com>
2021-09-24 23:58:19 +0600

    floating,double number and character practice

--- test.c
+++ test.c
@@ -12,10 +12,28 @@ int main()
     printf("The number is %d", num);
     */
 
-   int num1 = 10, num2 = 20;
+ /* write two numbers at the same time
+  int num1 = 10, num2 = 20;
 
     printf("Numbers are %d, %d\n",num1, num2);
    
-    return 0;
+   */
+    // float num1 = 20.3;
+    // double num2 = 33.44445;
+    // char ch = 'A';
+
+    // printf("Num1 = %.1f\n", num1);
+    // printf("Num2 = %lf\n", num2);
+    // printf("ch = %c", ch);
+
+    /*write a program which prints floating,double number and character.
+    */
+
+
+   
+
+   return 0;
+
+
 
 }
